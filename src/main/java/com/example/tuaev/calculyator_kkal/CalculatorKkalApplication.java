74aef045bc69108de4e9e41759296524872a6aca
Brown0602 <vladik.b2000@mail.ru>
2023-06-29 16:11:42 +0300

    d

--- src/main/java/com/example/tuaev/calculyator_kkal/CalculatorKkalApplication.java
+++ src/main/java/com/example/tuaev/calculyator_kkal/CalculatorKkalApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CalculatorKkalApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(CalculatorKkalApplication.class-class, args);
+		SpringApplication.run(CalculatorKkalApplication.class, args);
 
 	}
 }
